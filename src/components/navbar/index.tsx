c91da567d2008dd584113c0963fd9be463335778
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2024-08-24 14:12:36 +0700

    ✨ Update navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -6,10 +6,19 @@ import { mergeClassname } from "@utils/merge-classname";
 import useScrollPosition from "@hooks/use-scroll-position";
 import styles from "./index.module.css";
 import { COLOR_PALETTE } from "@utils/color";
+import { usePathname } from "next/navigation";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useRef } from "react";
 
 export default function Navbar() {
   const scrollPosition = useScrollPosition();
   const colors = Object.values(COLOR_PALETTE);
+  const pathName = usePathname();
+  const toggleRef = useRef<HTMLInputElement>(null);
+
+  const onNavClick = () => {
+    if (toggleRef.current) toggleRef.current.checked = false;
+  };
 
   return (
     <nav
@@ -19,25 +28,36 @@ export default function Navbar() {
       )}
     >
       <div className={styles.logo}>
-        <Link href="/">
+        <Link href="/" onClick={onNavClick}>
           <span style={{ color: colors.at(0) }}>W</span>
           <span style={{ color: colors.at(1) }}>W</span>
           <span style={{ color: colors.at(3) }}>WUTSQO</span>
         </Link>
       </div>
-      <div className={styles.links}>
-        {NAVIGATIONS.map((navigation, i) => (
-          <Link
-            className={styles.link}
-            key={navigation.path}
-            href={navigation.path}
-            style={{
-              color: colors[i % colors.length],
-            }}
-          >
-            {navigation.name}
-          </Link>
-        ))}
+      <div className={styles.linksWrapper}>
+        <label className={styles.burger}>
+          <input type="checkbox" ref={toggleRef} />
+          <Bars3Icon className={styles.iconOpen} />
+          <XMarkIcon className={styles.iconClose} />
+        </label>
+        <div className={styles.links}>
+          {NAVIGATIONS.map((navigation, i) => (
+            <Link
+              className={mergeClassname(
+                styles.link,
+                pathName === navigation.path ? styles.active : ""
+              )}
+              key={navigation.path}
+              href={navigation.path}
+              style={{
+                color: colors[i % colors.length],
+              }}
+              onClick={onNavClick}
+            >
+              {navigation.name}
+            </Link>
+          ))}
+        </div>
       </div>
     </nav>
   );
