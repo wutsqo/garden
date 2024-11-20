1f9ff67acc41e0686065232edecc7add70d42279
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2024-11-20 22:37:09 +0700

    ✨ Replace main text with Glowing component in Hero section

--- src/components/home/sections/hero.tsx
+++ src/components/home/sections/hero.tsx
@@ -1,19 +1,17 @@
 import Link from "next/link";
 import styles from "./hero.module.css";
 import Button from "@components/button";
+import Glowing from "@components/glowing";
 
 export default function Hero() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
         <span>Wutsqo is a </span>
-        <span className={styles.mainText} style={{ whiteSpace: "nowrap" }}>
-          Creative Developer
-        </span>
-        <span>
-          {" "}
-          who makes beautiful and functional digital products.
-        </span>
+        <Glowing>
+          <span>Creative Developer</span>
+        </Glowing>
+        <span> who makes beautiful and functional digital products.</span>
       </div>
 
       <Link href="/services" className={styles.buttonText}>
