9a0f9f62ebc6fdfc7ef623b1b594701b0a090380
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2024-11-20 22:34:19 +0700

    ♻️ Refactor heading components

--- src/components/mdx/headings/index.tsx
+++ src/components/mdx/headings/index.tsx
@@ -1,42 +1,41 @@
 import { FC, HTMLProps } from "react";
 import slugify from "@utils/slugify";
 
-interface HProps extends HTMLProps<HTMLHeadingElement> {}
+interface HProps extends HTMLProps<HTMLHeadingElement> {
+  level: 1 | 2 | 3 | 4 | 5 | 6; // Specify heading levels
+}
 
-const H1: FC<HProps> = ({ children, ...props }) => (
-  <h1 className="scroll-mt-24" id={slugify(children as string)}>
-    {children}
-  </h1>
+const Heading: FC<HProps> = ({ level, children, className, ...props }) => {
+  const Tag = `h${level}` as "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+  return (
+    <Tag
+      className={["scroll-mt-24", className].filter(Boolean).join(" ")}
+      id={slugify(children?.toString() ?? "")}
+      {...props}
+    >
+      {children}
+    </Tag>
+  );
+};
+
+const H1: FC<Omit<HProps, "level">> = (props) => (
+  <Heading level={1} {...props} />
 );
-
-const H2: FC<HProps> = ({ children, ...props }) => (
-  <h2 className="scroll-mt-24" id={slugify(children as string)}>
-    {children}
-  </h2>
+const H2: FC<Omit<HProps, "level">> = (props) => (
+  <Heading level={2} {...props} />
 );
-
-const H3: FC<HProps> = ({ children, ...props }) => (
-  <h3 className="scroll-mt-24" id={slugify(children as string)}>
-    {children}
-  </h3>
+const H3: FC<Omit<HProps, "level">> = (props) => (
+  <Heading level={3} {...props} />
 );
-
-const H4: FC<HProps> = ({ children, ...props }) => (
-  <h4 className="scroll-mt-24" id={slugify(children as string)}>
-    {children}
-  </h4>
+const H4: FC<Omit<HProps, "level">> = (props) => (
+  <Heading level={4} {...props} />
 );
-
-const H5: FC<HProps> = ({ children, ...props }) => (
-  <h5 className="scroll-mt-24" id={slugify(children as string)}>
-    {children}
-  </h5>
+const H5: FC<Omit<HProps, "level">> = (props) => (
+  <Heading level={5} {...props} />
 );
-
-const H6: FC<HProps> = ({ children, ...props }) => (
-  <h6 className="scroll-mt-24" id={slugify(children as string)}>
-    {children}
-  </h6>
+const H6: FC<Omit<HProps, "level">> = (props) => (
+  <Heading level={6} {...props} />
 );
 
 export { H1, H2, H3, H4, H5, H6 };
