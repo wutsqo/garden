d8a62b7beac1f1a289809d78d190c8a24abc4b7e
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2025-01-20 02:16:22 +0700

    ✨ Refactor ESLint configuration to use a variable for export

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,4 +10,5 @@ const compat = new FlatCompat({
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
 });
-export default [...compat.extends("next/core-web-vitals")];
+const config = [...compat.extends("next/core-web-vitals")];
+export default config;
