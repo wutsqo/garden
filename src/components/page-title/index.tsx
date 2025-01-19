c31a142c679cfb1b386c5c8ed1788abf5a0588a0
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2025-01-20 02:17:31 +0700

    ✨ Enhance PageTitle component to accept custom props for title and subtitle styling

--- src/components/page-title/index.tsx
+++ src/components/page-title/index.tsx
@@ -1,18 +1,26 @@
 import { FC, Fragment } from "react";
-import { PageTitleProps } from "./index.type";
 import Glowing from "@components/glowing";
+import { mergeClassname as cn } from "@utils/merge-classname";
+import { PageTitleProps } from "./index.type";
 
-const PageTitle: FC<PageTitleProps> = ({ title, subtitle, xl, notGlowing }) => {
+const PageTitle: FC<PageTitleProps> = ({ title, subtitle, xl, notGlowing, titleProps, subtitleProps }) => {
   const H1Wrapper = notGlowing ? Fragment : Glowing;
+  const tProps = {
+    ...titleProps,
+    className: cn("pb-1", titleProps?.className, xl ? "text-4xl sm:text-5xl" : "text-4xl"),
+  };
+  const sProps = {
+    ...subtitleProps,
+    className: cn("text-xl mt-2", subtitleProps?.className),
+  };
+
   return (
-    <div>
+    <Fragment>
       <H1Wrapper>
-        <h1 className={`pb-1 ${xl ? "text-4xl sm:text-5xl" : "text-4xl"}`}>
-          {title}
-        </h1>
+        <h1 {...tProps}>{title}</h1>
       </H1Wrapper>
-      {subtitle ? <div className="text-xl mt-2">{subtitle}</div> : null}
-    </div>
+      {subtitle ? <div {...sProps}>{subtitle}</div> : null}
+    </Fragment>
   );
 };
 
--- src/components/page-title/index.type.ts
+++ src/components/page-title/index.type.ts
@@ -1,6 +1,10 @@
+import { HTMLProps } from "react";
+
 export interface PageTitleProps {
   title: string;
   subtitle?: string;
   xl?: boolean;
   notGlowing?: boolean;
+  titleProps?: HTMLProps<HTMLHeadingElement>;
+  subtitleProps?: HTMLProps<HTMLDivElement>;
 }
