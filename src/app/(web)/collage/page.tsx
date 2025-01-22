5a073855747ecc3296c9593125e8e89e2d8d58bf
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2025-01-22 20:09:02 +0700

    ♻ Refactor Collage component styles and structure for improved layout and readability

--- src/app/(web)/collage/page.tsx
+++ src/app/(web)/collage/page.tsx
@@ -1,7 +1,6 @@
-import PageTitle from "@components/page-title";
 import Image from "next/image";
 import { getDistinctAlbums } from "./data";
-import s from "./page.module.css";
+import PageTitle from "@components/page-title";
 
 export const revalidate = 86400;
 
@@ -9,26 +8,19 @@ export default async function Collage() {
   const data = await getDistinctAlbums();
 
   return (
-    <div className={s.wrapper}>
-      <PageTitle
-        title="Collage 🎵"
-        subtitle="My top albums based on the last 4 weeks listening data from Spotify."
-      />
-      <div className={s.collage}>
-        {data.map((album) => (
-          <div key={album.id} className={s.collageItem}>
-            <Image
-              src={album.images[0].url}
-              alt={album.name}
-              fill
-              title={album.name}
-            />
-          </div>
-        ))}
+    <div className="animate-fade-in-up animation-delay-[300ms] relative z-50 grid grid-cols-2 bg-black opacity-0 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+      <div className="col-span-2 flex flex-col items-start justify-center p-8">
+        <PageTitle
+          title="Collage"
+          subtitle="Top albums based on recent Spotify listening history"
+          subtitleProps={{ className: "text-white mb-6" }}
+        />
       </div>
-      <div className={s.lastUpdated}>
-        Last updated: {new Date().toLocaleString("en-US")}
-      </div>
+      {data.slice(0, 34).map((album) => (
+        <div key={album.id} className="relative aspect-square">
+          <Image src={album.images[0].url} alt={album.name} fill title={album.name} />
+        </div>
+      ))}
     </div>
   );
 }
