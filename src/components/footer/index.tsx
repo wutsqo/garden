9ad2e5cda18aa10c4174b81f4ba0a49285c2bb1d
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2025-01-22 19:40:22 +0700

    ♻ Refactor Footer component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,15 +1,14 @@
-import s from "./index.module.css";
 import Mondrian from "@components/mondrian";
 
 export default function Footer() {
   return (
-    <footer className={s.wrapper}>
-      <div className={s.mondrianWrapper}>
+    <footer className="relative h-96 w-full bg-black text-white">
+      <div className="absolute -top-4 left-0 h-full w-full">
         <Mondrian keyPrefix="footer" />
       </div>
-      <div className={s.innerWrapper}>
-        <div className="flex items-center gap-2 h-12">
-          <div className="text-4xl -ml-3">✷</div>
+      <div className="flex h-full w-full flex-col items-start justify-end p-8 lg:flex-row lg:items-end lg:justify-between">
+        <div className="flex h-12 items-center gap-2">
+          <div className="-ml-3 text-4xl">✷</div>
           <div>
             <div>THE PERSONAL SITE & PORTFOLIO • WUTSQO</div>
           </div>
