7e1beaf8f8bcfbfa749d8bf4013788925fb00070
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2025-01-22 22:54:09 +0700

    ♻ Refactor About component layout for improved readability and structure

--- src/components/home/sections/about.tsx
+++ src/components/home/sections/about.tsx
@@ -12,13 +12,13 @@ export default function About() {
             <AboutMap />
           </div>
 
-          <div className="prose relative h-80 w-full max-w-full overflow-y-auto bg-white px-4 text-lg text-black md:px-8">
-            <p className="max-w-xl pt-4">
+          <div className="prose relative h-80 w-full max-w-full overflow-y-auto bg-white px-4 text-lg text-black md:px-8 flex flex-col justify-center">
+            <p className="max-w-xl">
               Hi, I am <strong>Wutsqo / Uko</strong> (he/him), a Jakarta-based Creative Developer. I indulge in{" "}
               <strong>web technologies</strong> both for fun and for a living.
             </p>
-            <p className="max-w-xl pt-4">
-              I also love to run, attend community events, and currently managing a local{" "}
+            <p className="max-w-xl">
+              I also love to run, attend community events, and currently an active member of a local{" "}
               <a href="https://www.instagram.com/makarabookclub/" target="_blank" rel="noopener noreferrer">
                 <strong>book club</strong>
               </a>{" "}
