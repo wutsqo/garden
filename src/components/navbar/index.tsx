0ff7881f0391972e0f10302b10ac8d4f7b9fc843
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2025-01-22 19:36:43 +0700

    ♻ Refactor Navbar component

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -7,8 +7,8 @@ import useScrollPosition from "@hooks/use-scroll-position";
 import styles from "./index.module.css";
 import { COLOR_PALETTE } from "@utils/color";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useRef } from "react";
+import { Menu, X } from "lucide-react";
 
 export default function Navbar() {
   const scrollPosition = useScrollPosition();
@@ -21,14 +21,9 @@ export default function Navbar() {
   };
 
   return (
-    <nav
-      className={mergeClassname(
-        styles.nav,
-        scrollPosition > 0 ? styles.navInverted : ""
-      )}
-    >
+    <nav className={mergeClassname(styles.nav, scrollPosition > 0 ? styles.navInverted : "")}>
       <div className={styles.logo}>
-        <Link href="/" onClick={onNavClick}>
+        <Link href="/" onClick={onNavClick} className="font-mono">
           <span style={{ color: colors.at(0) }}>W</span>
           <span style={{ color: colors.at(1) }}>W</span>
           <span style={{ color: colors.at(3) }}>WUTSQO</span>
@@ -37,16 +32,13 @@ export default function Navbar() {
       <div className={styles.linksWrapper}>
         <label className={styles.burger}>
           <input type="checkbox" ref={toggleRef} />
-          <Bars3Icon className={styles.iconOpen} />
-          <XMarkIcon className={styles.iconClose} />
+          <Menu className={styles.iconOpen} />
+          <X className={styles.iconClose} />
         </label>
         <div className={styles.links}>
           {NAVIGATIONS.map((navigation, i) => (
             <Link
-              className={mergeClassname(
-                styles.link,
-                pathName === navigation.path ? styles.active : ""
-              )}
+              className={mergeClassname(styles.link, pathName === navigation.path ? styles.active : "")}
               key={navigation.path}
               href={navigation.path}
               style={{
