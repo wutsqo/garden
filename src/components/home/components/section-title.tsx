27f48f50e8a4ddf4375a14e354af2448351cd10f
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2025-01-23 13:48:43 +0700

    🎨 Revamp Section Title

--- src/components/home/components/section-title.tsx
+++ src/components/home/components/section-title.tsx
@@ -7,11 +7,11 @@ interface Props {
 
 const SectionTitle: FC<Props> = ({ number, title }) => {
   return (
-    <div className="flex items-center justify-start gap-6 font-medium text-black">
-      <div className="shadow-brutalist flex h-12 w-12 items-center justify-center rounded border border-black bg-white text-lg">
+    <div className="divider divider-start divider-neutral flex items-center justify-start gap-6 font-medium text-black my-16">
+      <div className="shadow-brutalist flex h-10 w-10 shrink-0 items-center justify-center rounded border border-black bg-white text-base">
         {number}
       </div>
-      <div className="font-sans text-2xl">{title}</div>
+      <div className="font-sans text-2xl sm:text-3xl uppercase">{title}</div>
     </div>
   );
 };
