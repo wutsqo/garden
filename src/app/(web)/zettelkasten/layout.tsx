95a075ea9f02ae6e1a142d208fd08915c1681f0b
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2026-04-03 09:10:21 +0700

    🔥 Remove zettelkasten routes and note tooling

--- src/app/(web)/zettelkasten/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Layout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <div className="pt-16 container bg-white border-x min-h-screen">
-      {children}
-    </div>
-  );
-}
